6ce6c2e8db97613a8df900793fbf4a21fe8c1d2a
renanpmiranda <renanpmiranda5@gmail.com>
2023-01-10 15:20:30 -0300

    endpoints put e delete

--- src/index.ts
+++ src/index.ts
@@ -12,17 +12,25 @@ app.listen(3003, () => {
     console.log('Servidor rodando na porta 3003.')
 })
 
+// ENDPOINT DE TESTE
+
 app.get('/ping', (req: Request, res: Response) => {
     res.status(200).send('Pong!')
 })
+
+// GET ALL USERS
 
 app.get('/users', (req: Request, res: Response) => {
     res.status(200).send(users)
 })
 
+// GET ALL PRODUCTS
+
 app.get('/products', (req: Request, res: Response) => {
     res.status(200).send(products)
 })
+
+// GET PRODUCTS BY QUERY
 
 app.get('/products/search', (req: Request, res: Response) => {
     const q = req.query.q as string
@@ -33,6 +41,8 @@ app.get('/products/search', (req: Request, res: Response) => {
 
     res.status(200).send(response)
 })
+
+// CREATE USER
 
 app.post('/users', (req: Request, res: Response) => {
     const {id, email, password} = req.body as TUser
@@ -47,6 +57,8 @@ app.post('/users', (req: Request, res: Response) => {
 
     res.status(201).send('Cadastro realizado com sucesso.')
 })
+
+// CREATE PRODUCT
 
 app.post('/products', (req: Request, res: Response) => {
     const {id, name, price, category} = req.body as TProduct
@@ -64,6 +76,8 @@ app.post('/products', (req: Request, res: Response) => {
 
 })
 
+// CREATE PURCHASE
+
 app.post('/purchases', (req: Request, res: Response) => {
     const {userId, productId, quantity, totalPrice} = req.body as TPurchase
 
@@ -77,5 +91,102 @@ app.post('/purchases', (req: Request, res: Response) => {
     purchases.push(newPurchase)
 
     res.status(201).send('Compra realizada com sucesso.')
+})
 
+// GET PRODUCTS BY ID
+
+app.get('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id as string
+
+    const result = products.filter((product) => {
+        return product.id === id
+    })
+
+    res.status(200).send(result)
+})
+
+// GET USER PURCHASES BY ID
+
+app.get('/users/:id/purchases', (req: Request, res: Response) => {
+    const id = req.params.id as string
+
+    const userPurchases = purchases.filter((purchase) => {
+        return purchase.userId === id
+    })
+
+    res.status(200).send(userPurchases)
+})
+
+// DELETE USER BY ID
+
+app.delete('/users/:id', (req: Request, res: Response) => {
+    const id = req.params.id as string
+
+    const userIndex = users.findIndex((user) => {
+        return user.id === id
+    })
+
+    users.splice(userIndex, 1)
+
+    res.status(200).send('User apagado com sucesso.')
+})
+
+// DELETE PRODUCT BY ID
+
+app.delete('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id as string
+
+    const productIndex = products.findIndex((product) => {
+        return product.id === id
+    })
+
+    products.splice(productIndex, 1)
+
+    res.status(200).send('Produto apagado com sucesso.')
+})
+
+// EDIT USER BY ID
+
+app.put('/users/:id', (req: Request, res: Response) => {
+    const id = req.params.id as string
+
+    const newEmail = req.body.email as string | undefined
+    const newPassword = req.body.password as string | undefined
+
+    const userToBeEdited = users.find((user) => {
+        return user.id === id
+    })
+
+    if(userToBeEdited){
+        userToBeEdited.email = newEmail || userToBeEdited.email
+        userToBeEdited.password = newPassword || userToBeEdited.password
+
+        res.status(200).send('Cadastro atualizado com sucesso.')
+    }
+
+    res.status(404).send('Usuário não encontrado.')
+})
+
+// EDIT PRODUCT BY ID
+
+app.put('/products/:id', (req: Request, res: Response) => {
+    const id = req.params.id as string
+
+    const newName = req.body.name as string | undefined
+    const newPrice = req.body.price as number | undefined
+    const newCategory = req.body.category as string | undefined
+
+    const productToBeEdited = products.find((product) => {
+        return product.id === id
+    })
+
+    if(productToBeEdited){
+        productToBeEdited.name = newName || productToBeEdited.name
+        productToBeEdited.price = newPrice || productToBeEdited.price
+        productToBeEdited.category = newCategory || productToBeEdited.category
+
+        res.status(200).send('Produto atualizado com sucesso.')
+    }
+
+    res.status(404).send('Produto não encontrado.')
 })
